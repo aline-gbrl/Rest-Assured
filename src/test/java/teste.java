206cb0e0cafd0302c770b8b5d8900f1d8a734495
Aline Gabriele <aline_gbrl@outlook.com>
2026-09-02 18:39:17 -0500

    Subindo a Primeira Pipeline com Sucesso

--- src/test/java/teste.java
+++ src/test/java/teste.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class TesteBasico {
+public class teste {
 
     // Define um método de teste
     @Test
